673402b09cae7aaea03db706e101607a530aa8de
hogasuke <touhoufuhai.hogasuke@gmail.com>
2026-04-02 22:32:35 +0900

    ログイン画面のレイアウト適用

---
+++ src/app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'max:20'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'お名前を入力してください',
+            'name.string' => 'お名前を文字列で入力してください',
+            'name.max' => 'お名前を20文字以内で入力してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.string' => 'メールアドレスを文字列で入力してください',
+            'email.email' => 'メールアドレスはメール形式で入力してください',
+            'email.max' => 'メールアドレスは255文字以内でなければなりません',
+            'password.required' => 'パスワードを入力してください',
+            'password.string' => 'パスワードを文字列で入力してください',
+            'password.min' => 'パスワードは8文字以上で入力してください',
+            'password.confirmed' => 'パスワードと一致しません',
+        ];
+    }
+}
--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -5,48 +5,47 @@
 @endsection
 
 @section('content')
-    <div class="login-form__content">
-        <div class="login-form__heading">
-            <h2>ログイン</h2>
-        </div>
-        <form class="form" action="/login" method="post">
-            @csrf
-            <div class="form__group">
-                <div class="form__group-title">
-                    <span class="form__label--item">メールアドレス</span>
-                </div>
-                <div class="form__group-content">
-                    <div class="form__input--text">
-                        <input type="email" name="email" value="{{ old('email') }}" />
+    <div class="login">
+        <div class="login__container">
+            <div class="login__heading">
+                <h2 class="login__heading-title">ログイン</h2>
+            </div>
+
+            <form class="login-form" action="/login" method="post">
+                @csrf
+
+                <div class="login-form__group">
+                    <label class="login-form__label" for="email">メールアドレス</label>
+                    <div class="login-form__input">
+                        <input id="email" type="email" name="email" value="{{ old('email') }}">
                     </div>
-                    <div class="form__error">
+                    <div class="login-form__error">
                         @error('email')
                             {{ $message }}
                         @enderror
                     </div>
                 </div>
-            </div>
-            <div class="form__group">
-                <div class="form__group-title">
-                    <span class="form__label--item">パスワード</span>
-                </div>
-                <div class="form__group-content">
-                    <div class="form__input--text">
-                        <input type="password" name="password" />
+
+                <div class="login-form__group">
+                    <label class="login-form__label" for="password">パスワード</label>
+                    <div class="login-form__input">
+                        <input id="password" type="password" name="password">
                     </div>
-                    <div class="form__error">
+                    <div class="login-form__error">
                         @error('password')
                             {{ $message }}
                         @enderror
                     </div>
                 </div>
+
+                <div class="login-form__button">
+                    <button class="login-form__button-submit" type="submit">ログインする</button>
+                </div>
+            </form>
+
+            <div class="login__link">
+                <a class="login__link-register" href="/register">会員登録はこちら</a>
             </div>
-            <div class="form__button">
-                <button class="form__button-submit" type="submit">ログイン</button>
-            </div>
-        </form>
-        <div class="register__link">
-            <a class="register__button-submit" href="/register">会員登録の方はこちら</a>
         </div>
     </div>
 @endsection
