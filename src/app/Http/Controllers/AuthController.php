039ef0630ad3948806512632b39b7b0b9f5b31e7
hogasuke <touhoufuhai.hogasuke@gmail.com>
2026-04-05 22:20:04 +0900

    ログイン画面のバリデーション実装

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+
 
 class AuthController extends Controller
 {
     public function index()
     {
         return view('index');
+    }
+
+    public function login(LoginRequest $request): RedirectResponse
+    {
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect('/mypage/profile');
+        }
+        return back()->withErrors([
+            'email' => 'ログイン情報が登録されていません',
+        ])->onlyInput('email');
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+Route::middleware('guest')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/', [AuthController::class, 'index']);
     Route::get('/mypage/profile', function () {
