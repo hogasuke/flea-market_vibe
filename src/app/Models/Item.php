5fab472fe128cf01889cfff84de6d19d36ac9200
hogasuke <touhoufuhai.hogasuke@gmail.com>
2026-04-13 23:10:41 +0900

    モデルの作成

---
+++ src/app/Models/Item.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'brand_name',
+        'description',
+        'price',
+        'image_path',
+        'condition',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function purchases()
+    {
+        return $this->hasOne(Purchase::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'item_category');
+    }
+}
