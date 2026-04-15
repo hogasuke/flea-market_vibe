1294ae851f5b88ae7436979607feac7fc397a926
hogasuke <touhoufuhai.hogasuke@gmail.com>
2026-04-15 23:06:29 +0900

    商品一覧画面へのデータベース連携

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Item;
 
 class ItemController extends Controller
 {
     public function index()
     {
-        return view('items.index');
+        $items = Item::latest()->get();
+
+        return view('items.index', compact('items'));
     }
 
     public function show($item)
--- src/resources/views/items/index.blade.php
+++ src/resources/views/items/index.blade.php
@@ -5,10 +5,6 @@
 @endsection
 
 @section('content')
-    @php
-        $items = collect(range(1, 7))->map(fn($number) => ['name' => '商品名', 'id' => $number]);
-    @endphp
-
     <div class="item-index">
         <div class="item-index__tabs">
             <a class="item-index__tab item-index__tab--active" href="/">おすすめ</a>
@@ -17,10 +13,12 @@
 
         <div class="item-grid">
             @foreach ($items as $item)
-                <a class="item-card" href="{{ route('items.show', $item['id']) }}">
+                <a class="item-card" href="{{ route('items.show', $item->id) }}">
                     <article>
-                        <div class="item-card__image">商品画像</div>
-                        <p class="item-card__name">{{ $item['name'] }}</p>
+                        <div class="item-card__image">
+                            <img src="{{ $item->image_path }}" alt="{{ $item->name }}">
+                        </div>
+                        <p class="item-card__name">{{ $item->name }}</p>
                     </article>
                 </a>
             @endforeach
